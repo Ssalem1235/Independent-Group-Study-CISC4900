4fbddc48aaedf9c4f8a50832394e3a5f9235c907
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-30 01:37:37 -0400

    Update researchtab.js

--- tabs/researchtab.js
+++ tabs/researchtab.js
@@ -1,6 +1,8 @@
 //This is for our javascript code 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 ctx.font = "50px Arial";
 ctx.fillStyle = "brown";
 ctx.fillText("RESEARCH", 100, 200);
