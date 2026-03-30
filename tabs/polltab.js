0f61365f0e4724ec44b404dfa065733d1d963e95
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-30 01:27:37 -0400

    Update polltab.js

--- tabs/polltab.js
+++ tabs/polltab.js
@@ -1,5 +1,6 @@
 //This is for our javascript code 
-
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 ctx.font = "50px Arial";
 ctx.fillStyle = "brown";
 ctx.fillText("POLL", 100, 200);
