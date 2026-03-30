1d15fcb3d546e6162de1e443bfe35d401392f17c
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-30 19:07:17 -0400

    Delete tabs/polltab.js

--- tabs/polltab.js
+++
@@ -1,6 +0,0 @@
-//This is for our javascript code 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-ctx.font = "50px Arial";
-ctx.fillStyle = "brown";
-ctx.fillText("POLL", 100, 200);
