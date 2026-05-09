1fa85fcf7487d70de7c904a82ec0c15b1ec1b620
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-05-09 16:53:21 -0400

    Create polltab.js

---
+++ tabs/polltab.js
@@ -0,0 +1,24 @@
+const QUESTIONS = [
+  {
+    text: "What age group are you in?",
+    answers: ["Under 18", "18–24", "25–34", "35–44", "45+"]
+  },{
+    text: "Do you use AI for school or for work?",
+    answers: ["Yes", "No", "Both", "Personal Use"]
+  },
+  {
+    text: "Do you think AI has a more positive or negative affect on your life?(education/ convience/ resources)",
+    answers: ["Positive", "Negative"]
+  },
+  {
+    text: "Are you aware of any damage data centers have caused with our land, air, water?",
+    answers: ["Yes", "No"]
+  },{
+    text: "Are you aware of people using AI as an alternative for romantic and platonic relationships?",
+    answers: ["Yes", "No"]
+  },{
+    text: "Do you believe there will be a decline of jobs in the near future due to AI?",
+    answers: ["Yes", "No", "Not sure"]
+  }];
+
+  
