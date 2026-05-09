34e2d44e49298170ee3279d22b7cf61bdc543969
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-05-09 17:20:48 -0400

    Update polltab.js

--- tabs/polltab.js
+++ tabs/polltab.js
@@ -1,4 +1,4 @@
-const QUESTIONS = [
+const questions = [
   {
     text: "What age group are you in?",
     answers: ["Under 18", "18–24", "25–34", "35–44", "45+"]
@@ -21,4 +21,30 @@ const QUESTIONS = [
     answers: ["Yes", "No", "Not sure"]
   }];
 
+  const resultcolors = ['rgb(199, 21, 133)', 'rgb(255, 127, 80)', 'rgb(255, 215, 0)', 'rgb(106, 90, 205)', 'rgb(32, 178, 170)', 'rgb(128, 128, 0)'];
+
+let submissions = [];
+for(let i = 0; i < questions.length; i++){
+  submissions.push(new Array(questions[i].answers.length).fill(0));
+}
+let chosen = [];
+for(let i = 0; i < questions.length; i++) {
+  chosen.push(null);
+}
+
+function createPoll() {
+  const pollContainer = document.getElementById('poll');
+  pollContainer.inHTML = '';
+
+  for(let x = 0; x < questions.length; x++) {
+    let y = questions[x];
+
+    let box = document.createElement('div');
+    box.className = 'questionblock';
+
+    let optionsInHTML = '';
+    for(let a = 0; a < y.answers.length; a++) {
+      let b = y.answers[a];
+      optionsInHTML += 'button class= "optionbuttons" onclick"pick(' + x + ', ' + a + ', this)">' + b + '</button>';
+    }
   
